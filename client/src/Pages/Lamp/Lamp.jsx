6c3df694bfaef288d54884ef337800f710ac93fb
AgentSandbox <pregrada@mail.com>
2024-07-25 23:44:10 +0200

    commit1

--- client/src/Pages/Lamp/Lamp.jsx
+++ client/src/Pages/Lamp/Lamp.jsx
@@ -7,7 +7,6 @@ import {Menu} from "../Menu/Menu"
 
 const Chair = ({currentLang, menuOpened, setMenuOpened}) => {
     const [components, setComponents] = useState(StateLight)
-    const navigate = useNavigate()
     const clickFlipped = (id, check) => {
         setComponents(components.map(t => t.id === id ? {...t, check} : t))
     }
@@ -17,6 +16,11 @@ const Chair = ({currentLang, menuOpened, setMenuOpened}) => {
         setOpen(true)
         setFullScreenImgPath(pathFullScreenImg)
     }
+    const onClickImages = (id) => {
+        let newCount = components[id].imagesCounter
+        newCount > components[id].images.length-2 ? newCount = 0 : newCount++
+        setComponents(components.map(t => t.id === id ? {...t, imagesCounter: newCount} : t))
+    }
     const comp =
         components.map((i, index) => {
 
@@ -24,24 +28,33 @@ const Chair = ({currentLang, menuOpened, setMenuOpened}) => {
                 clickFlipped(i.id, !i.check)
             }
 
+
             return (
                 <div key={i.id}>
                     {i.foto ?
                         <ReactCardFlip isFlipped={i.check} flipDirection={"horizontal"}>
-                            <div className="card h-5/6 bg-white">
+                            <div className="card h-3/4 bg-white p-2">
                                 <div className="p-5 flex flex-col ">
                                     <div className="rounded-xl overflow-hidden ">
-                                        <img src={i.foto} alt=""
+                                        <img src={'./fotos/'+i.images[i.imagesCounter]} alt=""
                                              className="h-auto w-auto "
-                                             onClick={() => onClickFullScreenImg(i.foto)}
+                                             onClick={() => onClickFullScreenImg('./fotos/'+i.images[i.imagesCounter])}
                                         />
                                     </div>
-                                    <Link to={`/project/${i.path3D}/`}
-                                        // target="_blank"
-                                          className="h-12 text-xl text-white pt-1 text-center cursor-pointer bg-blue-600 rounded-lg"
-                                    >
-                                        <span className="text-3xs">3D models </span>
-                                    </Link>
+
+                                    <div className="h-auto flex flex-row gap-2 p-2">
+                                        <div
+                                            className="h-auto w-1/2 rounded-lg bg-gray-500 text-white content-center text-center text-2xl cursor-pointer"
+                                            onClick={() => onClickImages(i.id)}
+                                        >
+                                            IMAGES
+                                        </div>
+                                        <Link to={`/project/${i.path3D}/`}
+                                              className="h-12 w-1/2 text-2xl text-white pt-1 content-center text-center cursor-pointer bg-blue-600 rounded-lg"
+                                        >
+                                            <span className="text-3xs">3D models </span>
+                                        </Link>
+                                    </div>
                                     <a
                                         onClick={onClickButton}
                                         className="bg-gradient-to-r from-purple-500 to-pink-500 cursor-pointer h-12 text-center text-white py-2 rounded-lg text-xl font-semibold mt-4 hover:bg-blue-300 focus:scale-95 transition-all"
@@ -52,7 +65,7 @@ const Chair = ({currentLang, menuOpened, setMenuOpened}) => {
                                     </a>
                                 </div>
                             </div>
-                            <div className="card h-5/6 bg-white">
+                            <div className="card h-3/4 bg-white">
                                 <div className="p-5 flex flex-col">
                                     <div className="rounded-xl overflow-hidden xs:h-full">
                                         <img src={i.render}
@@ -88,19 +101,19 @@ const Chair = ({currentLang, menuOpened, setMenuOpened}) => {
 
     return (
         <>
-            <div className="mt-0 w-screen h-16 fixed fixed-row right-1 backdrop-blur-sm bg-gray-500/50  z-20"></div>
+            <div className="mt-0 w-screen h-16 fixed fixed-row right-1 backdrop-blur-sm bg-gray-500/50 z-20"></div>
             <Contacts/>
             <Menu menuOpened={menuOpened} setMenuOpened={setMenuOpened} currentLang={currentLang}/>
             <div className="flex items-center justify-center mx-auto h-auto w-screen bg-yellow-50">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-28 m-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-2 mt-12 gap-16">
 
                     {comp}
 
                     <div
                         onClick={() => setOpen(false)}
-                        className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visible bg-black/50" : "invisible"} `}
+                        className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visible bg-black/80" : "invisible"} `}
                     >
-                        <img src={fullScreenImgPath} alt="" className="h-full w-auto"/>
+                        <img src={fullScreenImgPath} alt="" className="md:h-full md:w-auto "/>
                     </div>
                 </div>
             </div>
--- client/src/state/stateLight.jsx
+++ client/src/state/stateLight.jsx
@@ -15,6 +15,10 @@ export const StateLight =
             nameRU: "Светильник бетонный",
             foto: "./fotos/lightConcreteRender2.png",
             render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
                 {
@@ -42,105 +46,225 @@ export const StateLight =
                     elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
                 },
             ]
-        },
-        {
+        },{
             id: 1,
-            price: "250",
+            price: "210",
             descriptionEN: "",
             descriptionRS: "",
             descriptionRU: "desctiption RU",
-            login3d: "lampwood",
-            pass3d: "demo",
-            nameEN: "LAMP WOOD",
-            nameRS: "ПОЛИЦА",
-            nameRU: "ЛАМПА ИЗ БРУСА",
-            foto: "./fotos/lamp1foto.png",
-            render: "./fotos/lamp2render.png",
+            path3D: 0,
+            login3d: "lamp",
+            nameEN: "Concrete lamp",
+            nameRS: "бетонска лампа",
+            nameRU: "Светильник бетонный",
+            foto: "./fotos/lightConcreteRender2.png",
+            render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
-                {id: v1(), groupElements: 0, name: "FoxChrome", file: "poligonFoxChrome", check: true},
-                {id: v1(), groupElements: 0, name: "FoxGold", file: "poligonFoxGold", check: false},
-                {id: v1(), groupElements: 0, name: "FoxBlack", file: "poligonFoxBlack", check: false},
+                {
+                    id: v1(),
+                    name: "Vendor code LC01",
+                    check: true,
+                    elems: ["lightConcrete1elem1", "lightConcrete1elem2", "lightConcrete1elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC02",
+                    check: false,
+                    elems: ["lightConcrete2elem1", "lightConcrete2elem2", "lightConcrete2elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC03",
+                    check: false,
+                    elems: ["lightConcrete3elem1", "lightConcrete3elem2", "lightConcrete3elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC04",
+                    check: false,
+                    elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
+                },
             ]
-        },
-        {
-            id: 1,
-            price: "250",
+        },{
+            id: 2,
+            price: "210",
             descriptionEN: "",
             descriptionRS: "",
             descriptionRU: "desctiption RU",
-            login3d: "lampwood",
-            pass3d: "demo",
-            nameEN: "LAMP WOOD",
-            nameRS: "ПОЛИЦА",
-            nameRU: "ЛАМПА ИЗ БРУСА",
-            foto: "./fotos/lamp1foto.png",
-            render: "./fotos/lamp2render.png",
+            path3D: 0,
+            login3d: "lamp",
+            nameEN: "Concrete lamp",
+            nameRS: "бетонска лампа",
+            nameRU: "Светильник бетонный",
+            foto: "./fotos/lightConcreteRender2.png",
+            render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
-                {id: v1(), groupElements: 0, name: "FoxChrome", file: "poligonFoxChrome", check: true},
-                {id: v1(), groupElements: 0, name: "FoxGold", file: "poligonFoxGold", check: false},
-                {id: v1(), groupElements: 0, name: "FoxBlack", file: "poligonFoxBlack", check: false},
+                {
+                    id: v1(),
+                    name: "Vendor code LC01",
+                    check: true,
+                    elems: ["lightConcrete1elem1", "lightConcrete1elem2", "lightConcrete1elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC02",
+                    check: false,
+                    elems: ["lightConcrete2elem1", "lightConcrete2elem2", "lightConcrete2elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC03",
+                    check: false,
+                    elems: ["lightConcrete3elem1", "lightConcrete3elem2", "lightConcrete3elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC04",
+                    check: false,
+                    elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
+                },
             ]
-        },
-        {
-            id: 1,
-            price: "250",
+        },{
+            id: 3,
+            price: "210",
             descriptionEN: "",
             descriptionRS: "",
             descriptionRU: "desctiption RU",
-            login3d: "lampwood",
-            pass3d: "demo",
-            nameEN: "LAMP WOOD",
-            nameRS: "ПОЛИЦА",
-            nameRU: "ЛАМПА ИЗ БРУСА",
-            foto: "./fotos/lamp1foto.png",
-            render: "./fotos/lamp2render.png",
+            path3D: 0,
+            login3d: "lamp",
+            nameEN: "Concrete lamp",
+            nameRS: "бетонска лампа",
+            nameRU: "Светильник бетонный",
+            foto: "./fotos/lightConcreteRender2.png",
+            render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
-                {id: v1(), groupElements: 0, name: "FoxChrome", file: "poligonFoxChrome", check: true},
-                {id: v1(), groupElements: 0, name: "FoxGold", file: "poligonFoxGold", check: false},
-                {id: v1(), groupElements: 0, name: "FoxBlack", file: "poligonFoxBlack", check: false},
+                {
+                    id: v1(),
+                    name: "Vendor code LC01",
+                    check: true,
+                    elems: ["lightConcrete1elem1", "lightConcrete1elem2", "lightConcrete1elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC02",
+                    check: false,
+                    elems: ["lightConcrete2elem1", "lightConcrete2elem2", "lightConcrete2elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC03",
+                    check: false,
+                    elems: ["lightConcrete3elem1", "lightConcrete3elem2", "lightConcrete3elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC04",
+                    check: false,
+                    elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
+                },
             ]
-        },
-        {
-            id: 1,
-            price: "250",
+        },{
+            id: 4,
+            price: "210",
             descriptionEN: "",
             descriptionRS: "",
             descriptionRU: "desctiption RU",
-            login3d: "lampwood",
-            pass3d: "demo",
-            nameEN: "LAMP WOOD",
-            nameRS: "ПОЛИЦА",
-            nameRU: "ЛАМПА ИЗ БРУСА",
-            foto: "./fotos/lamp1foto.png",
-            render: "./fotos/lamp2render.png",
+            path3D: 0,
+            login3d: "lamp",
+            nameEN: "Concrete lamp",
+            nameRS: "бетонска лампа",
+            nameRU: "Светильник бетонный",
+            foto: "./fotos/lightConcreteRender2.png",
+            render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
-                {id: v1(), groupElements: 0, name: "FoxChrome", file: "poligonFoxChrome", check: true},
-                {id: v1(), groupElements: 0, name: "FoxGold", file: "poligonFoxGold", check: false},
-                {id: v1(), groupElements: 0, name: "FoxBlack", file: "poligonFoxBlack", check: false},
+                {
+                    id: v1(),
+                    name: "Vendor code LC01",
+                    check: true,
+                    elems: ["lightConcrete1elem1", "lightConcrete1elem2", "lightConcrete1elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC02",
+                    check: false,
+                    elems: ["lightConcrete2elem1", "lightConcrete2elem2", "lightConcrete2elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC03",
+                    check: false,
+                    elems: ["lightConcrete3elem1", "lightConcrete3elem2", "lightConcrete3elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC04",
+                    check: false,
+                    elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
+                },
             ]
-        },
-        {
-            id: 1,
-            price: "250",
+        },{
+            id: 5,
+            price: "210",
             descriptionEN: "",
             descriptionRS: "",
             descriptionRU: "desctiption RU",
-            login3d: "lampwood",
-            pass3d: "demo",
-            nameEN: "LAMP WOOD",
-            nameRS: "ПОЛИЦА",
-            nameRU: "ЛАМПА ИЗ БРУСА",
-            foto: "./fotos/lamp1foto.png",
-            render: "./fotos/lamp2render.png",
+            path3D: 0,
+            login3d: "lamp",
+            nameEN: "Concrete lamp",
+            nameRS: "бетонска лампа",
+            nameRU: "Светильник бетонный",
+            foto: "./fotos/lightConcreteRender2.png",
+            render: "./fotos/lightConcreteRender1.png",
+            imagesCounter: 1,
+            images: [
+                "lightConcreteRender1.png","lightConcreteRender2.png", "lightConcreteRender3.png", "lightConcreteRender4.png"
+            ],
             check: true,
             components: [
-                {id: v1(), groupElements: 0, name: "FoxChrome", file: "poligonFoxChrome", check: true},
-                {id: v1(), groupElements: 0, name: "FoxGold", file: "poligonFoxGold", check: false},
-                {id: v1(), groupElements: 0, name: "FoxBlack", file: "poligonFoxBlack", check: false},
+                {
+                    id: v1(),
+                    name: "Vendor code LC01",
+                    check: true,
+                    elems: ["lightConcrete1elem1", "lightConcrete1elem2", "lightConcrete1elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC02",
+                    check: false,
+                    elems: ["lightConcrete2elem1", "lightConcrete2elem2", "lightConcrete2elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC03",
+                    check: false,
+                    elems: ["lightConcrete3elem1", "lightConcrete3elem2", "lightConcrete3elem3"]
+                },
+                {
+                    id: v1(),
+                    name: "Vendor code LC04",
+                    check: false,
+                    elems: ["lightConcrete4elem1", "lightConcrete4elem2"]
+                },
             ]
         },
     ]
